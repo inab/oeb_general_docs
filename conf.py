b6c9e26c3273ca8acb8b72044f1adf25ece6754b
José María Fernández <2072150+jmfernandez@users.noreply.github.com>
2021-04-06 12:51:52 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -34,8 +34,7 @@ release = '2020'
 extensions = [
     'recommonmark',
     'sphinx_markdown_tables',
-    'sphinx.ext.autosectionlabel',
-    'm2r2'
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
